35c11455dec23fe2520cbae8137ef6da6bf32d50
MATT | ADL | BLOCK #0080 <mattyoungaviation@gmail.com>
2026-06-26 10:31:39 -0700

    Fix CraftWorld config query

--- server/server.js
+++ server/server.js
@@ -86,7 +86,7 @@ const accountStatusQuery = `query AccountStatus { account { power powerMilliseco
 const proficienciesQuery = `query AccountProficiencies { account { proficiencies { symbol collectedAmount claimedLevel } } }`;
 const workshopQuery = `query AccountWorkshop { account { workshop { symbol level } } }`;
 const uidQuery = `query AccountUid { account { id walletAddress profile { uid walletAddress avatarUrl displayName } wallets { address type provider providerId primary } tradeAccount { tradeCount dailyRefillAmount totalTradeAmount capacity } } }`;
-const configQuery = `query FetchCraftWorld($uid: ID!) { fetchCraftWorld(uid: $uid) { landPlots { areas { symbol factories { factory { level definition { id } } } mines { level definition { id } } } } dynos { meta { displayName rarity } production { amount symbol } } resources { symbol amount } } }`;
+const configQuery = `query FetchCraftWorld($uid: ID!) { fetchCraftWorld(uid: $uid) { landPlots { areas { symbol factories { factory { level definition { id } } } } } dynos { meta { displayName rarity } production { amount symbol } } resources { symbol amount } } }`;
 
 function hms(ms) {
   const seconds = Math.max(0, Math.ceil(Number(ms || 0) / 1000));
@@ -125,7 +125,7 @@ const server = http.createServer(async (req, res) => {
   const origin = req.headers.origin;
   if (origin && getAllowedOrigins().includes(origin)) res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Vary', 'Origin');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-app-version');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   if (req.method === 'OPTIONS') return res.writeHead(204).end();
   try {
